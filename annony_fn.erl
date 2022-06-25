c32b866ac503ad42ac9221995abe7a62c96fe2a7
Nikola Vladimirov Iliev <nikiviliev98@gmail.com>
2022-06-25 12:10:49 +0300

    Delete erl files since this is forecast exam branch

--- annony_fn.erl
+++
@@ -1,7 +0,0 @@
--module(annony_fn).
--export([start/0]).
-
-start() ->
-    Fn = fun() ->
-        io:fwrite("Anonymous Function~n") end,
-    Fn().
--- guard_fn.erl
+++
@@ -1,8 +0,0 @@
--module(guard_fn).
--export([add/1,start/0]).
-
-add(X) when X>3 ->
-   io:fwrite("~w~n",[X]).
-
-start() ->
-   add(4).
